1ea3989bae44ddd9c582fe164ca7e772f5ded4b8
Prodip-Kumar-Paul <prodipkumarpaul708@gmail.com>
2022-02-15 20:17:30 +0530

    go back link added

--- src/utils/error.jsx
+++ src/utils/error.jsx
@@ -1,4 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
-    return <h1>404 not Found!</h1>;
-  };
+   return (
+      <>
+      <div className="flex justify-center dark:text-white">
+         <h1>404 not Found!</h1>
+         <Link className=" bg-green-600 m-5 p-2 rounded-full" to={"/"}>
+            Go Back
+         </Link>
+      </div>
+      </>
+   );
+};
 export default Error;

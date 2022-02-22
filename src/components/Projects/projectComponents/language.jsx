696439945efd995a666c643298666dfdc0b5f499
rahul garai <rahulgaraijuly2001@gmail.com>
2022-02-23 01:35:12 +0530

    feb-23-22/rahul/project_section_tags_and_updates

--- src/components/Projects/projectComponents/language.jsx
+++ src/components/Projects/projectComponents/language.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Data from "./data";
 
 export default function Language() {
-  const [accordian, setAccordian] = useState("");
+const [toggleSlide, setToggleSlide] = useState(false);
+useEffect(() => {
+  if(window.screen.width<=678) setToggleSlide(true);
+}, []);
   return (
     <>
-      <div className="mx-auto rounded-lg bg-neutral-100 max-w-[90%] border-2 my-2">
+      <div className="mx-auto rounded-lg max-w-[90%] border-2 my-2 dark:text-neutral-100">
         <section className="flex ">
           <div className="grow mx-3 my-2">Language</div>
           <span
-            onClick={() => setAccordian(!accordian)}
-            className="cursor-pointer"
+            onClick={() => setToggleSlide(!toggleSlide)}
+            className="cursor-pointer text-center"
           >
             🔽
           </span>
         </section>
-        <section className="max-h-[15rem] overflow-auto">{accordian || <Data />}</section>
+        <section className="max-h-[15rem] overflow-auto">{toggleSlide || <Data />}</section>
       </div>
     </>
   );

746f8419f25d616b961cf8732090a8fc0d12f57d
Prodip-Kumar-Paul <prodipkumarpaul708@gmail.com>
2022-02-27 18:14:18 +0530

    few changes

---
+++ src/components/Projects/VideoUploader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FileDropzone from "../../layouts/FileDropzone";
+
+export default function VideoUploader() {
+   return (
+      <div>
+         <h2>Upload a Video</h2>
+         <FileDropzone />
+      </div>
+   );
+}
